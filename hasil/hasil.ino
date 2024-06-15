d6ce79dbcf670af285487237d8603f6c6f4c481c
Aryasharii <105221013@student.universitaspertamina.ac.id>
2024-06-15 14:29:14 +0700

    upload hasil

--- hasil/hasil.ino
+++ hasil/hasil.ino
@@ -1,55 +1,64 @@
+#include <DHT.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <DHT.h>
 
-// Constants
-#define DHTPIN 32
-#define DHTTYPE DHT11   
-#define WIFI_SSID "Madrasah Dalam UPer"
-#define WIFI_PASS "Dalambanget17"
-#define SERVER_IP "192.168.100.5"
-#define SERVER_PORT 80
-#define API_ENDPOINT "/api/data"
+const char* ssid = "Kos Bu Yayan";
+const char* password = "Bangunpagi123";
+const char* serverName = "http://192.168.1.21:5000/api/sensor";
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(15, DHT11);
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  
+  WiFi.begin(ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
+  
   Serial.println("Connected to WiFi");
 }
 
 void loop() {
-  delay(2000);
-  float hum = dht.readHumidity();
-  float temp = dht.readTemperature();
-  
-  if (isnan(hum) || isnan(temp)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  
-  // Send data to server
-  HTTPClient http;
-  http.begin("http://" + String(SERVER_IP) + ":" + String(SERVER_PORT) + API_ENDPOINT);
-  http.addHeader("Content-Type", "application/json");
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
 
-  String postData = "{\"temperature\": " + String(temp) + ", \"humidity\": " + String(hum) + "}";
-  int httpResponseCode = http.POST(postData);
+    float temp = dht.readTemperature();
+    float humidity = dht.readHumidity();
 
-  if (httpResponseCode > 0) {
-    String response = http.getString();
-    Serial.println("HTTP Response code: " + String(httpResponseCode));
-    Serial.println(response);
+    if (isnan(temp) || isnan(humidity)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    String outputTemp = "Temp: " + String(temp) + " C";
+    String outputHumidity = "Humidity: " + String(humidity) + " %";
+    Serial.println(outputTemp);
+    Serial.println(outputHumidity);
+
+    String postData = "{\"temperature\":" + String(temp) + ", \"humidity\":" + String(humidity) + "}";
+
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+
+    int httpResponseCode = http.POST(postData);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
   } else {
-    Serial.println("Error sending data to server!");
-    Serial.println(httpResponseCode);
+    Serial.println("Error in WiFi connection");
   }
 
-  http.end();
-  delay(10000);
+  delay(2000); 
+}
